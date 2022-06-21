591b75269ae7a5ba63eb1fa879dccf9267b31636
Dkukf <derekleung0806@hotmail.com>
2022-06-21 18:57:03 +0100

    Added GridObject Abstract Class
    Modified the Obstacle and SelfDrivingVehicle classes so that they both extend the GridObject abstract class.

--- Obstacle.java
+++ Obstacle.java
@@ -1,35 +1,12 @@
 // This template is used to create Obstacle objects for the AmazonWorkSample
 // class.
-public class Obstacle
+public class Obstacle extends GridObject
 {
-  // Declare the variables for the position of the obstacle.
-  private int xPosition;
-  private int yPosition;
-
   // Constructor method for the Obstacle object.
   public Obstacle(int givenX, int givenY)
   {
     // Store the given coordinates into the variables of the object.
-    xPosition = givenX;
-    yPosition = givenY;
+    setX(givenX);
+    setY(givenY);
   } // Obstacle method
-
-  // This method returns the x-coordinate of the obstacle.
-  public int getX()
-  {
-    return xPosition;
-  } // giveX method
-
-  // This method returns the y-coordinate of the obstacle.
-  public int getY()
-  {
-    return yPosition;
-  } // giveY method
-
-  // The toString method returns the coordinates of the obstacle.
-  public String toString()
-  {
-    return "(" + Integer.toString(xPosition) + ", "
-           + Integer.toString(yPosition) + ")";
-  } // toString method
 } // Obstacle class
--- SelfDrivingVehicle.java
+++ SelfDrivingVehicle.java
@@ -1,17 +1,13 @@
 // This template is used to create a self-driving vehicle object for the
 // AmazonWorkSample class.
-public class SelfDrivingVehicle
+public class SelfDrivingVehicle extends GridObject
 {
-  // Declare the variables for the position of the vehicle.
-  private int xPosition;
-  private int yPosition;
-
   // This is the constructor method of the self-driving vehicle object.
   public SelfDrivingVehicle()
   {
     // Initially, the position of the vehicle is at (0, 0).
-    xPosition = 0;
-    yPosition = 0;
+    setX(0);
+    setY(0);
   } // selfDrivingVehicle method
 
   // This method is used to move the self-driving vehicle in a diagonal
@@ -24,55 +20,48 @@ public class SelfDrivingVehicle
       // If the vehicle is moving to the southeast, then add 1 to both the
       // x position and y position variables.
       case "Southeast":
-        xPosition++;
-        yPosition++;
+        setX(getX() + 1);
+        setY(getY() + 1);
         break;
       // If the vehicle is moving to the northeast, then add 1 to the x
       // position variable and subtract 1 from the y position variable.
       case "Northeast":
-        xPosition++;
-        yPosition--;
+        setX(getX() + 1);
+        setY(getY() - 1);
         break;
       // If the vehicle is moving to the southwest, then subtract 1 from the x
       // position variable and add 1 to the y position variable.
       case "Southwest":
-        xPosition--;
-        yPosition++;
+        setX(getX() - 1);
+        setY(getY() + 1);
         break;
       // If the vehicle is moving northwest, then subtract 1 from both the x
       // and y position variables.
       case "Northwest":
-        xPosition--;
-        yPosition--;
+        setX(getX() - 1);
+        setY(getY() - 1);
         break;
       // If the vehicle moves east, then add 1 to the x position variable.
       case "East":
-        xPosition++;
+        setX(getX() + 1);
         break;
       // If the vehicle moves south, then add 1 to the y position variable.
       case "South":
-        yPosition++;
+        setY(getY() + 1);
         break;
       // If the vehicle moves west, then subtract 1 from the x position
       // variable.
       case "West":
-        xPosition--;
+        setX(getX() - 1);
         break;
       // If the vehicle moves north, then subtract 1 from the y position
       // variable.
       case "North":
-        yPosition--;
+        setY(getY() - 1);
         break;
       // If the instruction was unclear, then do nothing.
       default:
         break;
     } // switch
   } // move method
-
-  // This method reports the location of the vehicle object when invoked.
-  public String toString()
-  {
-    return "(" + Integer.toString(xPosition) + ", "
-           + Integer.toString(yPosition) + ")";
-  } // toString method
 } // SelfDrivingVehicle class
