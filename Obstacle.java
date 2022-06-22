426199296a8ef848222fc972f400b55a285732ca
Dkukf <derekleung0806@hotmail.com>
2022-06-22 01:21:58 +0100

    Removed the GridObject and SelfDrivingVehicle Classes
    The SelfDrivingVehicle object was replaced with an Obstacle object, which made both the GridObject and SelfDrivingVehicle classes redundant.

--- Obstacle.java
+++ Obstacle.java
@@ -1,7 +1,11 @@
 // This template is used to create Obstacle objects for the AmazonWorkSample
 // class.
-public class Obstacle extends GridObject
+public class Obstacle
 {
+  // Declare the variables for the position of the obstacle object.
+  private int xPosition;
+  private int yPosition;
+
   // Constructor method for the Obstacle object.
   public Obstacle(int givenX, int givenY)
   {
@@ -9,4 +13,35 @@ public class Obstacle extends GridObject
     setX(givenX);
     setY(givenY);
   } // Obstacle method
+
+  // This method returns the x-coordinate of the obstacle object.
+  public int getX()
+  {
+    return xPosition;
+  } // giveX method
+
+  // This method returns the y-coordinate of the obstacle object.
+  public int getY()
+  {
+    return yPosition;
+  } // giveY method
+
+  // This method sets the x-coordinate of the obstacle object.
+  public void setX(int givenX)
+  {
+    xPosition = givenX;
+  } // setX method
+
+  // This method sets the y-coordinate of the obstacle object.
+  public void setY(int givenY)
+  {
+    yPosition = givenY;
+  } // setX method
+
+  // This method reports the location of the obstacle object when invoked.
+  public String toString()
+  {
+    return "(" + Integer.toString(xPosition) + ", "
+           + Integer.toString(yPosition) + ")";
+  } // toString method
 } // Obstacle class
